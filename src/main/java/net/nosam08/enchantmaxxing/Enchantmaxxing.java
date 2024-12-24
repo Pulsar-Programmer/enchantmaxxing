6cb10623e4a9b3438f1c59dba3cba4683b1a1983
Pulsar-Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-12-24 15:52:58 -0500

    Initialize Repository with Guess on Gitignore

---
+++ src/main/java/net/nosam08/enchantmaxxing/Enchantmaxxing.java
@@ -0,0 +1,16 @@
+package net.nosam08.enchantmaxxing;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Enchantmaxxing implements ModInitializer {
+	public static final String MOD_ID = "enchantmaxxing";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/net/nosam08/enchantmaxxing/EnchantmaxxingClient.java
@@ -0,0 +1,13 @@
+package net.nosam08.enchantmaxxing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantmaxxingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
