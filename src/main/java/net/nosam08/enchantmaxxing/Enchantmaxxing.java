a96e7919a21fef4522f38c430871334e827bf1f7
Pulsar-Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2025-07-11 13:59:25 -0400

    Change Change in game name from Enchantmaxxing to Enchantify Fixes #15 ID only

--- src/main/java/net/nosam08/enchantmaxxing/Enchantmaxxing.java
+++ src/main/java/net/nosam08/enchantmaxxing/Enchantmaxxing.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 // import net.nosam08.enchantmaxxing.config.EnchantifyConfig;
 
 public class Enchantmaxxing implements ModInitializer {
-	public static final String MOD_ID = "enchantmaxxing";
+	public static final String MOD_ID = "enchantify";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	// public static EnchantifyConfig CONFIG;
