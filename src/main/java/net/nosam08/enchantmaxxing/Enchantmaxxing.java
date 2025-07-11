bd3e1c3d16dd3ec5c25b93323bb4facc4896fdfe
Pulsar-Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2025-07-10 20:10:21 -0400

    Update and start some dependencies

--- src/main/java/net/nosam08/enchantmaxxing/Enchantmaxxing.java
+++ src/main/java/net/nosam08/enchantmaxxing/Enchantmaxxing.java
@@ -5,12 +5,21 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// import me.shedaniel.autoconfig.AutoConfig;
+// import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+// import net.nosam08.enchantmaxxing.config.EnchantifyConfig;
+
 public class Enchantmaxxing implements ModInitializer {
 	public static final String MOD_ID = "enchantmaxxing";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// public static EnchantifyConfig CONFIG;
+
 	@Override
 	public void onInitialize() {
-		
+		// AutoConfig.register(EnchantifyConfig.class, JanksonConfigSerializer::new);
+        // CONFIG = AutoConfig.getConfigHolder(EnchantifyConfig.class).getConfig();
+
+        // System.out.println("Particles enabled: " + CONFIG.enableParticles);
 	}
 }
---
+++ src/main/java/net/nosam08/enchantmaxxing/config/EnchantifyConfig.java
@@ -0,0 +1,11 @@
+package net.nosam08.enchantmaxxing.config;
+
+// import me.shedaniel.autoconfig.ConfigData;
+// import me.shedaniel.autoconfig.annotation.Config;
+
+
+// @Config(name = "mymod")
+// public class EnchantifyConfig implements ConfigData {
+//     public boolean enableParticles = true;
+//     public int particleCount = 64;
+// }
