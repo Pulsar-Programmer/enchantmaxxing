865e737725cb3a0b866a1cdf2db909c38fe29ac7
Pulsar-Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2025-07-25 00:28:36 -0400

    tiny Skelecodes

--- src/main/java/net/nosam08/enchantmaxxing/EnchantifyClient.java
+++ src/main/java/net/nosam08/enchantmaxxing/EnchantifyClient.java
@@ -5,8 +5,12 @@ import org.lwjgl.glfw.GLFW;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.texture.TextureStitcher.Slot;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.item.ItemStack;
 
 public class EnchantifyClient implements ClientModInitializer {
 
@@ -29,5 +33,17 @@ public class EnchantifyClient implements ClientModInitializer {
         // });
         
     }
+
+
+    // public ItemStack detect_hovered_item(MinecraftClient client){
+
+    //     if(client.currentScreen == null){
+    //         return ItemStack.EMPTY;
+    //     }
+
+    //     if(client.currentScreen instanceof HandledScreen<?> handledScreen){
+            
+    //     }
+    // }
     
 }
--- src/main/java/net/nosam08/enchantmaxxing/menu/EnchantmaxOrganizer.java
+++ src/main/java/net/nosam08/enchantmaxxing/menu/EnchantmaxOrganizer.java
@@ -1,9 +1,18 @@
 package net.nosam08.enchantmaxxing.menu;
 
+import net.nosam08.enchantmaxxing.menu.ds.CompatibilityChart;
+
 /** Finds all the possible in-game enchantments and their compatabilites. */
 public class EnchantmaxOrganizer {
     
-    private static void build_from_start(){
+    private static CompatibilityChart build_from_start(){
+        //stream registries
+        var reg_len = 10;
 
+        for(var i = 0; i < reg_len; i++){
+            
+        }
+
+        return null; //TODO
     }
 }
