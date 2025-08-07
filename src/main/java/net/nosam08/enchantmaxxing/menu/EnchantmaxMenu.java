92e31ae9423d98877f6cdd52ddcd246d9ca0fe24
Pulsar-Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2025-08-07 16:05:02 -0400

    Make Progress on Enchant Menu

--- src/main/java/net/nosam08/enchantmaxxing/menu/EnchantmaxMenu.java
+++ src/main/java/net/nosam08/enchantmaxxing/menu/EnchantmaxMenu.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 import org.jetbrains.annotations.NotNull;
 
+import com.google.common.collect.Lists;
+
 import io.wispforest.owo.ui.base.BaseOwoScreen;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.container.ScrollContainer;
 import io.wispforest.owo.ui.core.Component;
 import io.wispforest.owo.ui.core.HorizontalAlignment;
 import io.wispforest.owo.ui.core.Insets;
@@ -30,22 +33,50 @@ public class EnchantmaxMenu extends BaseOwoScreen<FlowLayout> {
     @Override
     protected void build(FlowLayout rootComponent) {
         rootComponent
-                    .surface(Surface.VANILLA_TRANSLUCENT)
-                    .horizontalAlignment(HorizontalAlignment.CENTER)
-                    .verticalAlignment(VerticalAlignment.TOP);
+            .surface(Surface.VANILLA_TRANSLUCENT)
+            .horizontalAlignment(HorizontalAlignment.CENTER)
+            .verticalAlignment(VerticalAlignment.TOP);
+
+        var buttons = enchant_buttons(Lists.newArrayList("Sharpness", "Silk Touch", "Potato Monkeys", "Potato Monkeys", "Potato Monkeys", "Potato Monkeys", "Potato Monkeys", "Potato Monkeys", "Potato Monkeys", "Potato Monkeys"));
+
+        var flow = Containers.verticalFlow(Sizing.content(), Sizing.content())
+            .children(buttons)
+            .verticalAlignment(VerticalAlignment.CENTER)
+            .horizontalAlignment(HorizontalAlignment.CENTER)
+            .margins(Insets.horizontal(3)
+        );
 
         rootComponent.child(
-                Containers.verticalFlow(Sizing.content(), Sizing.content())
-                    .child(Components.button(Text.literal("A Button"), button -> { System.out.println("click"); }))
-                    .child(Components.button(Text.literal("A Button"), button -> { System.out.println("click"); }))
-                    .child(Components.button(Text.literal("A Button"), button -> { System.out.println("click"); }))
-                    .child(Components.button(Text.literal("A Button"), button -> { System.out.println("click"); }))
-                    .padding(Insets.of(10))
-                    .surface(Surface.DARK_PANEL)
-                    .verticalAlignment(VerticalAlignment.CENTER)
-                    .horizontalAlignment(HorizontalAlignment.CENTER)
-                    .margins(Insets.top(20))
+            Containers.verticalScroll(Sizing.content(), Sizing.fill(), 
+                flow
+            )
+            .scrollbarThiccness(4)
+            .scrollbar(ScrollContainer.Scrollbar.vanilla())
+            .padding(Insets.of(10))
+            .surface(Surface.DARK_PANEL)
+            .verticalAlignment(VerticalAlignment.CENTER)
+            .horizontalAlignment(HorizontalAlignment.CENTER)
+            .margins(Insets.vertical(10))
         );
+    }
+
+    public ArrayList<Component> enchant_buttons(ArrayList<String> names){
+        ArrayList<Component> array = new ArrayList<>();
+        names.forEach(x -> {
+            array.add(enchant_button(x));
+        });
+        return array;
+    }
+
+
+
+    public Component enchant_button(String name){
+        return Components.button(Text.literal(name), button -> {
+            //button function
+            // button.active = false;
+            button.active = !button.active;
+            System.out.println("click");
+        }).margins(Insets.of(3));
     }
 
 
